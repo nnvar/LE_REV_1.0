00b700e8765ecf4d582f0f5275cddeaf97c62ea1
nnvar <ubbercube4@gmail.com>
2024-06-02 14:42:45 -0500

    saved

--- lets-eat/src/App.js
+++ lets-eat/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import EatForm from './components/EatForm';
+//import EatForm from './components/EatForm';
 import EatList from './components/EatList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
     <div className='letsEat'>
+      <NavbarComp/>
       <EatList/>
     </div>
   );

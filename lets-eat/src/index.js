30ea2b529d590f9fc53d0e7c14b3f35e75ef9735
nnvar <ubbercube4@gmail.com>
2024-06-17 18:24:54 -0500

    ?

--- lets-eat/src/index.js
+++ lets-eat/src/index.js
@@ -1,6 +1,27 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App';
+import List from './pages/List';
+import Home from './pages/Home';
+import Layout from "./pages/Layout";
+
+function App() {
+  return ( 
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+              <Route index element={<Home />}/>
+              <Route path="list" element={<List/>}/>
+          </Route>
+        </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ lets-eat/src/pages/List.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import EatList from '../components/EatList'
+
+function List() {
+  return (
+    <EatList/>
+  )
+}
+
+export default List
